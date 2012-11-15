85665c48c3ccb13a3170470a88848f1322ae8076
hhagmans <hendrikh@innoq.com>
2012-11-15 18:22:26 +0100

    ManytoMany relatin now bidirectional

--- src/com/innoq/sample/JpaDemo.java
+++ src/com/innoq/sample/JpaDemo.java
@@ -1,7 +1,5 @@
 package com.innoq.sample;
 
-import java.sql.Date;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,12 +13,44 @@ public class JpaDemo {
 		try {
 			Address address = new Address("Germany","Monheim","Krischerstr.");
 			User user = new User("Test",false);
+			User user2 = new User("Test2",false);
+			User user3 = new User("Test3",false);
+			User user4 = new User("Test4",false);
+			User user5 = new User("Test5",false);
 			//Event event1 = new Event("Pommes", "", Date.valueOf("2012-10-17"), 10, false);
 			em.getTransaction().begin();
 			user.setAddress(address);
+			user2.setAddress(address);
+			user3.setAddress(address);
+			user4.setAddress(address);
+			user5.setAddress(address);
 			Project project = new Project("Projekt1");
+			Project project2 = new Project("Projekt2");
 			user.addProject(project);
+			user2.addProject(project2);
+			user3.addProject(project);
+			user4.addProject(project2);
+			user5.addProject(project);
+			user.addProject(project2);
+			user2.addProject(project);
+			user3.addProject(project2);
+			user4.addProject(project);
+			user5.addProject(project2);
+			project.addUser(user);
+			project.addUser(user2);
+			project.addUser(user3);
+			project.addUser(user4);
+			project.addUser(user5);
+			project2.addUser(user);
+			project2.addUser(user2);
+			project2.addUser(user3);
+			project2.addUser(user4);
+			project2.addUser(user5);
 			em.persist(user);
+			em.persist(user2);
+			em.persist(user3);
+			em.persist(user4);
+			em.persist(user5);
 			//em.persist(event1);
 			em.getTransaction().commit();
 
@@ -35,6 +65,10 @@ public class JpaDemo {
 			for (User u : em.createQuery(query, User.class).getResultList()){
 				System.out.println(u.getAddress());
 				System.out.println(u.getProjects());
+			}
+			query = "SELECT p FROM Project p";
+			for (Project p : em.createQuery(query, Project.class).getResultList()){
+				System.out.println(p.getUsers());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
--- src/com/innoq/sample/Project.java
+++ src/com/innoq/sample/Project.java
@@ -1,10 +1,14 @@
 package com.innoq.sample;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.Table;
@@ -26,8 +30,20 @@ private static final long serialVersionUID = 1L;
 	@Column(name = "name")
 	private String name;
 	
+	@ManyToMany(mappedBy="projects")
+	private List<User> users;
+	
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public void addUser(User user) {
+		this.users.add(user);
+	}
+
 	public Project(String name){
 		this.name = name;
+		this.users = new ArrayList<User>();
 	}
 	
 	public Project(){
