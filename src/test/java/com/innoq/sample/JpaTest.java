e11ecdffed0fe19706ba06119d75a36548030371
hhagmans <hendrikh@innoq.com>
2012-11-16 14:38:30 +0100

    entity creation in test moved into testcases

--- src/test/java/com/innoq/sample/JpaTest.java
+++ src/test/java/com/innoq/sample/JpaTest.java
@@ -12,6 +12,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,14 +22,24 @@ import org.junit.Test;
  */
 public class JpaTest {
 
+	EntityManagerFactory emf;
+	EntityManager em;
 	/**
 	 * @throws java.lang.Exception
 	 */
 	@Before
-	public void setUp() throws Exception {
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("myJPAProject");
-		EntityManager em = emf.createEntityManager();
-		
+	public void setUp() throws Exception {	
+		emf = Persistence.createEntityManagerFactory("myJPAProject");
+		em = emf.createEntityManager();
+	}
+
+	@After
+    public void tearDown() {
+        em.clear();
+    }
+	
+	@Test
+	public void oneToOneTest() {
 		try {
 		Address address = new Address("Germany","Monheim","Krischerstr.");
 		User user = new User("Test",false);
@@ -36,16 +47,6 @@ public class JpaTest {
 		User user3 = new User("Test3",false);
 		User user4 = new User("Test4",false);
 		User user5 = new User("Test5",false);
-		
-		Event event1 = new Event("Pommes", "", Date.valueOf("2012-10-17"), 10, false);
-		
-		Booking booking1 = new Booking(event1, user, false);
-		Booking booking2 = new Booking(event1, user2, false);
-		Booking booking3 = new Booking(event1, user3, false);
-		
-		event1.addBooking(booking1);
-		event1.addBooking(booking2);
-		event1.addBooking(booking3);
 		
 		em.getTransaction().begin();
 		
@@ -55,47 +56,19 @@ public class JpaTest {
 		user4.setAddress(address);
 		user5.setAddress(address);
 		
-		Project project = new Project("Projekt1");
-		Project project2 = new Project("Projekt2");
-		
-		user.addProject(project);
-		user2.addProject(project2);
-		user3.addProject(project);
-		user4.addProject(project2);
-		user5.addProject(project);
-		user.addProject(project2);
-		user2.addProject(project);
-		user3.addProject(project2);
-		user4.addProject(project);
-		user5.addProject(project2);
-		
-		project.addUser(user);
-		project.addUser(user2);
-		project.addUser(user3);
-		project.addUser(user4);
-		project.addUser(user5);
-		project2.addUser(user);
-		project2.addUser(user2);
-		project2.addUser(user3);
-		project2.addUser(user4);
-		project2.addUser(user5);
-		
 		em.persist(user);
 		em.persist(user2);
 		em.persist(user3);
 		em.persist(user4);
 		em.persist(user5);
-		em.persist(event1);
 		
 		em.getTransaction().commit();
+		
 		} catch (Exception e) {
 			e.printStackTrace();
 			em.getTransaction().rollback();
 		}
-	}
-
-	@Test
-	public void oneToOneTest() {
+		
 		String query = "SELECT u FROM User u";
 		for (User u : em.createQuery(query, User.class).getResultList()){
 			assertEquals("Germany Monheim Krischerstr.", u.getAddress());
@@ -104,6 +77,44 @@ public class JpaTest {
 	
 	@Test
 	public void oneToManyTest() {
+		try {
+			Address address = new Address("Germany","Monheim","Krischerstr.");
+			User user = new User("Test",false);
+			User user2 = new User("Test2",false);
+			User user3 = new User("Test3",false);
+			User user4 = new User("Test4",false);
+			User user5 = new User("Test5",false);
+			
+			Event event1 = new Event("Pommes", "", Date.valueOf("2012-10-17"), 10, false);
+			
+			Booking booking1 = new Booking(event1, user, false);
+			Booking booking2 = new Booking(event1, user2, false);
+			Booking booking3 = new Booking(event1, user3, false);
+			
+			em.getTransaction().begin();
+			
+			user.setAddress(address);
+			user2.setAddress(address);
+			user3.setAddress(address);
+			user4.setAddress(address);
+			user5.setAddress(address);
+			
+			event1.addBooking(booking1);
+			event1.addBooking(booking2);
+			event1.addBooking(booking3);
+			
+			em.persist(user);
+			em.persist(user2);
+			em.persist(user3);
+			em.persist(event1);
+			
+			em.getTransaction().commit();
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+				em.getTransaction().rollback();
+			}
+		
 		String query = "SELECT e FROM Event e";
 		for (Event e : em.createQuery(query, Event.class).getResultList()){
 			assertEquals(3, e.getBookings().size());
@@ -112,6 +123,61 @@ public class JpaTest {
 	
 	@Test
 	public void manyToManyTest() {
+		try {
+			Address address = new Address("Germany","Monheim","Krischerstr.");
+			User user = new User("Test",false);
+			User user2 = new User("Test2",false);
+			User user3 = new User("Test3",false);
+			User user4 = new User("Test4",false);
+			User user5 = new User("Test5",false);
+			
+			em.getTransaction().begin();
+			
+			user.setAddress(address);
+			user2.setAddress(address);
+			user3.setAddress(address);
+			user4.setAddress(address);
+			user5.setAddress(address);
+			
+			Project project = new Project("Projekt1");
+			Project project2 = new Project("Projekt2");
+			
+			user.addProject(project);
+			user2.addProject(project2);
+			user3.addProject(project);
+			user4.addProject(project2);
+			user5.addProject(project);
+			user.addProject(project2);
+			user2.addProject(project);
+			user3.addProject(project2);
+			user4.addProject(project);
+			user5.addProject(project2);
+			
+			project.addUser(user);
+			project.addUser(user2);
+			project.addUser(user3);
+			project.addUser(user4);
+			project.addUser(user5);
+			project2.addUser(user);
+			project2.addUser(user2);
+			project2.addUser(user3);
+			project2.addUser(user4);
+			project2.addUser(user5);
+			
+			em.persist(user);
+			em.persist(user2);
+			em.persist(user3);
+			em.persist(user4);
+			em.persist(user5);
+			
+			em.getTransaction().commit();
+			
+			} catch (Exception e) {
+				e.printStackTrace();
+				em.getTransaction().rollback();
+			}
+
+		
 		String query = "SELECT u FROM User u";
 		for (User u : em.createQuery(query, User.class).getResultList()){
 			assertEquals(2,u.getProjects().size());
