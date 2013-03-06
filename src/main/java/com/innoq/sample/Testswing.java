4ed363903209ec13ece4ec4584b0ad7ca00e0c67
hhagmans <hendrikh@innoq.com>
2013-03-06 13:48:48 +0100

    Upload File and Download File implemented in Client

--- src/main/java/com/innoq/sample/Testswing.java
+++ src/main/java/com/innoq/sample/Testswing.java
@@ -3,10 +3,18 @@ package com.innoq.sample;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.ObjectOutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +22,17 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.swing.*;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.apache.commons.io.FileUtils;
 
 import com.google.gson.Gson;
 class JavaSwing1{
@@ -65,11 +84,11 @@ class JavaSwing1{
 			eventdetails[3] = "Details: " + event.getDetails();
 			eventdetails[4] = "Slots: " + event.getSlots();
 			eventdetails[5] = "Vegetarian: " + event.isVegetarian_opt();
-			String[] bookings = new String[50];
+			String book = "Bookings: ";
 			for (int i = 0; i < event.getBookings().size(); i++) {
-				bookings[i] = event.getBookings().get(i).getUser().getShortname();
+				book = book + event.getBookings().get(i).getUser().getShortname() + ", ";
 			}
-			eventdetails[6] = "Bookings: " + bookings.toString();
+			eventdetails[6] = book;
 			JList auswahl = new JList(eventdetails);
 			return auswahl;
 		}
@@ -78,6 +97,60 @@ class JavaSwing1{
 			JList auswahl = new JList(err);
 			return auswahl;
 		}
+	}
+	
+	public void fileUpload(JButton component, JFrame frame) throws ClientProtocolException, IOException{
+		final JFileChooser fc = new JFileChooser();
+		fc.setSelectedFile(new File(System.getProperty("user.dir")));
+		int returnVal = fc.showOpenDialog(component);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+            File file = fc.getSelectedFile();
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost("http://localhost:8080/myJPAProject/App/fileservlet/upload");
+
+            MultipartEntity entity = new MultipartEntity();
+            entity.addPart("file", new FileBody(file));
+            post.setEntity(entity);
+
+            HttpResponse response = client.execute(post);
+            HttpEntity responseEntity = response.getEntity();
+            String body = "";
+            if(responseEntity!=null) {
+                body = EntityUtils.toString(responseEntity);
+            }
+            JLabel statuslabel = new JLabel(body);
+            if (frame.getContentPane().getComponentCount() > 4) {
+        		frame.getContentPane().remove(4);
+        		}
+            frame.getContentPane().add(statuslabel);
+            frame.setVisible(true);
+        } else {
+            
+        }
+	}
+	
+	public void saveFile(JButton component, JFrame frame) throws MalformedURLException, IOException{
+		JFileChooser c = new JFileChooser();
+	    c.setSelectedFile(new File("~/mytext.txt"));
+	      int rVal = c.showSaveDialog(component);
+	      if (rVal == JFileChooser.APPROVE_OPTION) {
+	        String filename = c.getSelectedFile().getName();
+	        String dir = c.getCurrentDirectory().toString();
+	        try {
+	        FileUtils.copyURLToFile(new URL("http://localhost:8080/myJPAProject/App/fileservlet/mytext.txt"), new File(dir + "/" + filename));
+	        JLabel statuslabel = new JLabel("Datei gespeichert");
+            if (frame.getContentPane().getComponentCount() > 4) {
+        		frame.getContentPane().remove(4);
+        		}
+            frame.getContentPane().add(statuslabel);
+            frame.setVisible(true);
+	        }
+	        catch (IOException e1) {
+				e1.printStackTrace();
+			}
+	      }
+	      if (rVal == JFileChooser.CANCEL_OPTION) {
+	}
 	}
 	
 	public void display(){
@@ -109,6 +182,20 @@ class JavaSwing1{
           };
           eventButton.addActionListener( eventlist );
           
+          final JButton uploadButton = new JButton("File Upload");
+          ActionListener fileUpload = new ActionListener() {
+              public void actionPerformed( ActionEvent e ) {
+            	  try {
+					fileUpload(uploadButton, jfrm);
+				} catch (ClientProtocolException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+              }
+            };
+            uploadButton.addActionListener( fileUpload );
+            
           JButton homeButton = new JButton("Zurück zu Home");
           ActionListener home = new ActionListener() {
               public void actionPerformed( ActionEvent e ) {
@@ -116,10 +203,26 @@ class JavaSwing1{
               }
             };
           homeButton.addActionListener( home );
+          
+          final JButton saveButton = new JButton("Speichere File");
+          ActionListener saveFile = new ActionListener() {
+              public void actionPerformed( ActionEvent e ) {
+              	try {
+					saveFile(saveButton, jfrm);
+				} catch (MalformedURLException e1) {
+					e1.printStackTrace();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+              }
+            };
+          saveButton.addActionListener( saveFile );
         
-          jfrm.getContentPane().add(eventIDField);
+        jfrm.getContentPane().add(eventIDField);
         jfrm.getContentPane().add(eventButton);
-
+        jfrm.getContentPane().add(uploadButton);
+        jfrm.getContentPane().add(saveButton);
+        
 		jfrm2.setSize(400,400);
 
 		jfrm2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
